f88c3c05f12f5e701571a6d2b1e931a35ebc199f
zna.kshi <148896028+znakshi@users.noreply.github.com>
2024-09-13 13:26:29 +0800

    Delete webpage3 directory

--- webpage3/scripts.js
+++
@@ -1,9 +0,0 @@
-// Select all images in the gallery
-const images = document.querySelectorAll('section img');
-
-// Add a click event listener to each image
-images.forEach(image => {
-    image.addEventListener('click', () => {
-        alert(`You clicked on: ${image.alt}`);
-    });
-});
